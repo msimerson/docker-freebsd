6e24f14797b5ff03d6a831b7942113d3807d4e99
Akihiro Uchida <uchida@turbare.net>
2016-05-26 21:00:30 +0900

    use https to download tar archives

--- fetch_release.sh
+++ fetch_release.sh
@@ -5,10 +5,10 @@ set -eux -o pipefail
 
 WORKDIR="$(dirname "$(pwd)/$0")"
 pushd "$WORKDIR"
-  curl -O http://ftp.freebsd.org/pub/FreeBSD/releases/amd64/${RELVER}/base.txz
+  curl -O https://download.freebsd.org/ftp/releases/amd64/${RELVER}/base.txz
   BASE_SHA256SUM=$(awk '/^base.txz/ {print $2}' MANIFEST)
   [ $BASE_SHA256SUM = $(openssl dgst -sha256 base.txz | awk '{print $2}') ]
   LIB32_SHA256SUM=$(awk '/^lib32.txz/ {print $2}' MANIFEST)
-  curl -O http://ftp.freebsd.org/pub/FreeBSD/releases/amd64/${RELVER}/lib32.txz
+  curl -O https://download.freebsd.org/ftp/releases/amd64/${RELVER}/lib32.txz
   [ $LIB32_SHA256SUM = $(openssl dgst -sha256 lib32.txz | awk '{print $2}') ]
 popd
